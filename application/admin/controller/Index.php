e44f617e88b4a0702e47f432019d858a471a16ab
zhangjun <zhangjun@qq.com>
2018-12-11 22:28:04 +0800

    修改123

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -2,11 +2,19 @@
 namespace app\admin\controller;
 
 use think\Controller;
+use lib\map\Map;
 
 class Index extends Controller
 {
     public function index()
     {
+        $map = new Map();
+
+        $address = '北京市海淀区上地十街10号';
+
+        $data = $map::getLocation($address);
+        get_prints_r($data,1);
+
       return $this->fetch();
     }
 
